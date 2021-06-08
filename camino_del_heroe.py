74977b13bd9d7f40cceeeaf215aefaa43ca23b85
fededomgom <60011087+fededomgom@users.noreply.github.com>
2021-06-08 19:13:06 -0300

    Agregué varias funciones, corregí el bug del daño

--- camino_del_heroe.py
+++ camino_del_heroe.py
@@ -10,10 +10,9 @@ espada_daño = 10
 precio_guantes = randint(1,20)
 precio_daga = randint(30,40)
 precio_espada = randint(70,90)
-
 def camino1(nombre):
     print(nombre + " decidiste tomar el Camino 1... Sabia eleccion. Aunque lamentablemente la última")
-    print("Te come un lobo salvaje.")
+    print("Te come una manada de lobos salvajes.")
 
 def camino2(nombre, cantidad_oro):
     oro_recibido = randint(1,50)
@@ -69,8 +68,6 @@ def tienda(nombre, cantidad_oro, inventario):
         print(inventario)
         arma_heroe = espada_daño
         return arma_heroe
-    else:
-        print("El vendedor te mata por no comprar nada, mal ahí.")
 
 def orco(nombre, cantidad_oro, inventario, arma_heroe, vida_heroe):
     print("Te encontraste con un orco, que desafortunado, " + nombre + ".")
@@ -78,7 +75,7 @@ def orco(nombre, cantidad_oro, inventario, arma_heroe, vida_heroe):
     vida_orco = randint(30,50)
     print("Cada golpe de este orco hace " + str(daño_orco) + " de daño. Además, tiene " + str(vida_orco) + " puntos de vida." )
     print("Afortunadamente, traes contigo tu " + str(inventario) + " y tienes " + str(vida_heroe) + " puntos de vida.")
-    while vida_heroe > 0 or vida_orco > 0:
+    while vida_heroe > 0 and vida_orco > 0:
         print("Quieres golpear al orco o huir? (Pelear/Huir)")
         respuesta_orco = input()
         if respuesta_orco == "Pelear":
@@ -86,6 +83,33 @@ def orco(nombre, cantidad_oro, inventario, arma_heroe, vida_heroe):
             vida_heroe = vida_heroe - daño_orco
             print("Sos más rápido que el orco, asi que pegas primero, causandole " + str(arma_heroe)+ " de daño, dejandalo con " + str(vida_orco) + " de vida.")
             print("El orco te pega con sus puños, causandote " + str(daño_orco) + " de daño, y dejandote con " + str(vida_heroe) + " de vida.")
+    if vida_heroe < 0:
+        print("Te mató el orco, fallaste")
+        sys.exit()
+    elif vida_orco < 0:
+        print("Mataste al orco, bien ahí")
+        monedas_orco = randint(1,50)
+        print("El orco tenía " + str(monedas_orco) + " monedas, las cuales te llevas")
+        cantidad_oro += monedas_orco
+        print("Ahora tenes " + str(cantidad_oro) + " monedas.")
+
+
+def cura(nombre, vida_heroe, cantidad_oro):
+    cantidad_vida_cura = randint(15,50)
+    print("Te encontraste con un amigable cura")
+    print("Te ofrece curarte " +  str(cantidad_vida_cura) + " puntos de vida por " + str(cantidad_vida_cura) + " monedas de oro." )
+    print("Tienes " + str(vida_heroe) + " puntos de vida y " + str(cantidad_oro) + " monedas de oro. Quieres curarte? (si/no)")
+    respuesta_cura = input()
+    if respuesta_cura == "si":
+        print("El cura, utilizando sus poderes arcanos, te cura")
+        cantidad_oro = cantidad_oro - cantidad_vida_cura
+        vida_heroe += cantidad_vida_cura
+        print("Ahora tenés " + str(cantidad_oro) +" monedas de oro y " + str(vida_heroe) + " puntos de vida.")
+    else:
+        print("El cura es bastante caprichoso, y no va a aceptar un no como respuesta.")
+        print("El cura te roba 10 monedas usando su magia, y te saca la lengua (no literalmente) :P")
+        cantidad_oro = cantidad_oro - 10
+        print("Ahora tenés " + str(cantidad_oro) + " monedas de oro.")
 
 while True:
     while True:
@@ -109,9 +133,12 @@ while True:
                 arma_heroe = tienda(nombre, cantidad_oro, inventario)
                 print("Luego de comprar tu arma en la tienda, sigues por tu camino, determinado.")
                 orco(nombre, cantidad_oro, inventario, arma_heroe, vida_heroe)
+                print("Luego de semejante batalla, te encuentras agotado.")
+                cura(nombre, vida_heroe, cantidad_oro)
         elif jugar_o_no == "no":
             print("Por las barbas de Merlin, que maleducad@!")
             break
         break
     break
-# by fede..........
+
+# by chancho
